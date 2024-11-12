c4f0fd51cb9e9e87d17928e63f1fecec8d11f798
FPLCodes <sahabferdous1@gmail.com>
2024-11-13 00:28:02 +0800

    Update skeleton design for NFT gallery

--- src/components/NFTGallery.tsx
+++ src/components/NFTGallery.tsx
@@ -74,18 +74,41 @@ export default function NFTGallery({
 
   if (isLoading) {
     return (
-      <div className="p-4">
-        <Skeleton className="h-8 w-1/3 mb-4" />
-        <div className={`grid ${getGridClasses()}`}>
-          {[...Array(10)].map((_, index) => (
-            <div key={index} className="overflow-hidden">
-              <Skeleton className="w-full pt-[110%]" />
-              <div className="p-4 h-[90px]">
-                <Skeleton className="h-6 w-3/4 mb-2" />
-                <Skeleton className="h-4 w-full" />
-              </div>
-              <div className="p-2">
-                <Skeleton className="h-4 w-1/2" />
+      <div className="p-4 mt-10">
+        {/* Top section skeleton */}
+        <div className="flex justify-between items-center mb-6">
+          {/* View mode buttons skeleton */}
+          <div className="flex space-x-2">
+            {[...Array(3)].map((_, index) => (
+              <Skeleton key={index} className="h-9 w-9 rounded-md" />
+            ))}
+          </div>
+
+          {/* Search bar skeleton */}
+          <div className="flex items-center w-1/2">
+            <Skeleton className="w-full h-10 rounded-r-none" />
+            <Skeleton className="h-10 w-10 rounded-l-none" />
+          </div>
+
+          {/* Mint button skeleton */}
+          <Skeleton className="h-10 w-32 rounded-md" />
+        </div>
+
+        {/* Accordion items skeleton */}
+        <div className="space-y-4">
+          {[...Array(2)].map((_, index) => (
+            <div key={index}>
+              <Skeleton className="h-8 w-1/4 mb-2 rounded-md" />
+              <div className={`grid ${getGridClasses()}`}>
+                {[...Array(5)].map((_, index) => (
+                  <div key={index} className="overflow-hidden">
+                    <Skeleton className="w-full pt-[110%] rounded-lg" />
+                    <div className="p-4 h-[90px] space-y-2">
+                      <Skeleton className="h-6 w-3/4 rounded-md" />
+                      <Skeleton className="h-4 w-1/2 rounded-md" />
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
           ))}
